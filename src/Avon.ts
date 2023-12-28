fa8bfb51b0f71b6b5a35ccdb333f572a8da8c343
Ismail Zare <zarehesmaiel@gmail.com>
2023-12-28 18:58:03 +0330

    Update Avon.ts

--- src/Avon.ts
+++ src/Avon.ts
@@ -29,7 +29,7 @@ export default class Avon {
   /**
    * Indicates application current version.
    */
-  protected static VERSION = '1.2.4';
+  protected static VERSION = '1.2.5';
 
   /**
    * Array of available resources.
