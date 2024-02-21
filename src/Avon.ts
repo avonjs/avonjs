678afda5e042c9d1ae0cb067ab40c7ab86f31467
Ismail Zare <zarehesmaiel@gmail.com>
2024-02-21 21:08:54 +0330

    bump version

--- src/Avon.ts
+++ src/Avon.ts
@@ -29,7 +29,7 @@ export default class Avon {
   /**
    * Indicates application current version.
    */
-  protected static VERSION = '2.2.0';
+  protected static VERSION = '2.2.1';
 
   /**
    * Array of available resources.
